73a55d5f676d8e358540fedfd5e24ba970c9aacf
abhishek94443 <abhishekdwivedi9444@gmail.com>
2026-06-24 23:03:12 +0530

    Strategy Pattern

---
+++ src/com/systemdesign/LLD/Patterns/BehaviouralPatterns/StrategyPattern/GoodCode/Main.java
@@ -0,0 +1,8 @@
+package com.systemdesign.LLD.Patterns.BehaviouralPatterns.StrategyPattern.GoodCode;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Strategy Pattern
+    }
+}
